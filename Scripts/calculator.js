d0f6cf8992315bdce0df0f9ca9cc6e417ae5167c
Lelouch223311 <foracademy228@mail.ru>
2023-09-10 20:24:40 +0300

    6 modified | 10 New Files

---
+++ Scripts/calculator.js
@@ -0,0 +1,15 @@
+// =============== [ В Calculator.html Стоит НЕ МОЙ Код !! Код был взят с StackOverflow[RU] !]
+
+// let a1 = +prompt('Введите число номер 1: ', '5')
+// let b1 = +prompt('Введите число номер 2: ', '5')
+// let result;
+
+// function calculate(){
+
+//     return result = a + b;
+// }
+// function callback1(calculate){
+
+//     return console.log('Результат(+): ', result)
+// }
+// callback1(calculate())
